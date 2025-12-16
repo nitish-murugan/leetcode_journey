1943db51d6d7af961c64a86be09c4c2724bfc5b0
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2025-12-16 10:01:22 +0530

    Time: 2 ms (93.38%), Space: 45.6 MB (47.22%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,27 @@
 class Solution {
-    public void fn(int i, int[] candidate, int target, List<List<Integer>> arr, List<Integer> temp){
-        if(i==candidate.length){
-            if(target==0){
-                arr.add(new ArrayList<>(temp));
+    public void fn(int i, int[] candidates, int target, List<List<Integer>> lst, List<Integer> temp){
+        if(i == candidates.length){
+            if(target == 0){
+                lst.add(new ArrayList<>(temp));
             }
             return;
         }
-        if(candidate[i]<=target){
-            temp.add(candidate[i]);
-            fn(i,candidate,target-candidate[i],arr,temp);
+        if(target == 0){
+            lst.add(new ArrayList<>(temp));
+            return;
+        }
+        if(candidates[i]<=target){
+            temp.add(candidates[i]);
+            fn(i, candidates, target-candidates[i], lst, temp);
             temp.remove(temp.size()-1);
         }
-        fn(i+1,candidate,target,arr,temp);
-
+        if(i<candidates.length){
+            fn(i+1, candidates, target, lst, temp);
+        }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> lst = new ArrayList<>();
-        fn(0,candidates,target,lst,new ArrayList<>());
+        fn(0, candidates, target, lst, new ArrayList<>());
         return lst;
     }
 }
