4accb517466ab6299e1c0fff65647bfe2f5962de
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2025-12-18 09:25:09 +0530

    Time: 1 ms (89%), Space: 45.2 MB (71.89%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void fn(int[] nums, List<List<Integer>> lst, List<Integer> temp, boolean[] arr){
+    public void fn(int[] nums, boolean[] arr, List<List<Integer>> lst, List<Integer> temp){
         if(temp.size() == nums.length){
             lst.add(new ArrayList<>(temp));
             return;
@@ -9,18 +9,16 @@ class Solution {
             if(!arr[i]){
                 temp.add(nums[i]);
                 arr[i] = true;
-                fn(nums,lst,temp,arr);
+                fn(nums,arr,lst,temp);
                 temp.remove(temp.size()-1);
                 arr[i] = false;
             }
         }
     }
-    
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> lst = new ArrayList<>();
         boolean[] arr = new boolean[nums.length];
-        Arrays.fill(arr,false);
-        fn(nums,lst,new ArrayList<>(),arr);
+        fn(nums,arr,lst,new ArrayList<>());
         return lst;
     }
 }
