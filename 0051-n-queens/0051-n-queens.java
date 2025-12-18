d175c7130f8df1a70be736603a32c5bdc3704ed0
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2025-12-18 09:48:33 +0530

    Time: 9 ms (7.95%), Space: 48.3 MB (5.03%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,23 +1,24 @@
 class Solution {
-    public void fn(int col, int n, List<List<String>> lst, ArrayList<String> temp, HashSet<Integer> rows, HashSet<Integer> dia1, HashSet<Integer> dia2){
-        if(col==n){
+    public void fn(int col, int n, List<List<String>> lst, List<String> temp, HashSet<Integer> rows, HashSet<Integer> dia1, HashSet<Integer> dia2){
+        
+        if(col == n){
             lst.add(new ArrayList<>(temp));
             return;
         }
 
         for(int row=0;row<n;row++){
             if(rows.contains(row) || dia1.contains(row-col) || dia2.contains(row+col)) continue;
+            
             rows.add(row);
+            String tempStr = "";
+            for(int i=0;i<n;i++){
+                if(i==row) tempStr+='Q';
+                else tempStr+='.';
+            }
+
+            temp.add(tempStr);
             dia1.add(row-col);
             dia2.add(row+col);
-            String res = "";
-            for(int i=0;i<n;i++){
-                if(i==row) res+='Q';
-                else res+='.';
-            }
-            temp.add(res);
-
-            
 
             fn(col+1,n,lst,temp,rows,dia1,dia2);
             temp.remove(temp.size()-1);
@@ -25,6 +26,7 @@ class Solution {
             dia1.remove(row-col);
             dia2.remove(row+col);
         }
+
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> lst = new ArrayList<>();
