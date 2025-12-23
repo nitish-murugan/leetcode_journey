f0d2dfcd1c88634676c586c6b740588af8fbcde1
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2025-12-23 19:58:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,18 +1,28 @@
-// User function Template for Java
 class Solution {
     int fn(int i, int[] height, int[] dp){
-        if(i==0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int leftOne = fn(i-1,height,dp)+ Math.abs(height[i]-height[i-1]);
-        int leftTwo = Integer.MAX_VALUE;
-        if(i>1) leftTwo = fn(i-2,height,dp)+ Math.abs(height[i]-height[i-2]);
-        dp[i] = Math.min(leftOne,leftTwo);
+        if(i == height.length-1){
+            return 0;
+        }
+        
+        if(dp[i] != -1) return dp[i];
+        
+        int first = Integer.MAX_VALUE;
+        if(i+1 < height.length){
+            first = Math.abs(height[i]-height[i+1]) + fn(i+1, height, dp);
+        }
+        
+        int second = Integer.MAX_VALUE;
+        if(i+2 < height.length){
+            second = Math.abs(height[i]-height[i+2]) + fn(i+2, height, dp);
+        }
+        
+        dp[i] = Math.min(first, second);         
         return dp[i];
     }
     int minCost(int[] height) {
         // code here
         int[] dp = new int[height.length+1];
-        Arrays.fill(dp,-1);
-        return fn(height.length-1,height,dp);
+        Arrays.fill(dp, -1);
+        return fn(0, height, dp);
     }
 }
