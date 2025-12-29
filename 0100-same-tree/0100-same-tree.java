100a2452c94e5d09c4d149ec0ded7fe6d3085b85
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2025-12-29 20:53:31 +0530

    Time: 0 ms (100%), Space: 43.3 MB (10.38%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,8 +16,14 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
+        if(p==null && q!=null) return false;
+        if(p!=null && q==null) return false;
         if(p.val != q.val) return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        if(p.left==null && q.left!=null) return false;
+        if(p.left!=null && q.left==null) return false;
+        if(p.right==null && q.right!=null) return false;
+        if(p.right!=null && q.right==null) return false;
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
