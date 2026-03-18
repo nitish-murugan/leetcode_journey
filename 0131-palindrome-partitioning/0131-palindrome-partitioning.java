19bd4d6850946e73125b78904ddaa2a3c1d9242e
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2026-03-18 19:43:29 +0530

    Time: 8 ms (74.75%), Space: 65.2 MB (70.85%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,29 +1,31 @@
 class Solution {
-    public void fn(String s, int i, List<List<String>> lst, List<String> temp){
-        if(i==s.length()){
-            lst.add(new ArrayList<>(temp));
-            return;
-        }
-
-        for(int index=i;index<s.length();index++){
-            if(isPalindrome(s,i,index)){
-                temp.add(s.substring(i,index+1));
-                fn(s,index+1,lst,temp);
-                temp.remove(temp.size()-1);
-            }
-        }
-    }
-    public boolean isPalindrome(String s, int i, int j){
+    public boolean palindrome(int i, int j, String str){
         while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
+            if(str.charAt(i) != str.charAt(j)) return false;
             i++;
             j--;
         }
         return true;
     }
+
+    public void fn(int i, String s, int length, List<List<String>> lst, List<String> temp){
+        if(i == length){
+            lst.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int j=i;j<length;j++){
+            if(palindrome(i,j,s)){
+                temp.add(s.substring(i,j+1));
+                fn(j+1, s, length, lst, temp);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+
     public List<List<String>> partition(String s) {
         List<List<String>> lst = new ArrayList<>();
-        fn(s, 0, lst, new ArrayList<>());
+        fn(0, s, s.length(), lst, new ArrayList<>());
         return lst;
     }
 }
